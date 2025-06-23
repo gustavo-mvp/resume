1be6f5dfd18876648814e8d871bb1cff3b48e216
Gustavo Martins Vasconcellos <gustavo@mvprojects.dev>
2025-06-23 11:56:05 -0300

    feat: add mulish font

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,12 +4,23 @@ export default function App({ Component }: PageProps) {
     <html>
       <head>
         <meta charset="utf-8" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap"
+          rel="stylesheet"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>resume</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
-        <Component />
+        <main>
+          <Component />
+        </main>
       </body>
     </html>
   );
